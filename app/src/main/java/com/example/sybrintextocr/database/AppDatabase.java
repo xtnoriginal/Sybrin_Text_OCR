aeca6a0f31a04c14587ffda740898ad79cba3b41
Claudious  Nhemwa <xtnoriginal@icloud.com>
2022-07-14 08:16:45 +0200

    added get data from the database for UI updates

--- app/src/main/java/com/example/sybrintextocr/database/AppDatabase.java
+++ app/src/main/java/com/example/sybrintextocr/database/AppDatabase.java
@@ -2,9 +2,11 @@ package com.example.sybrintextocr.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,12 +26,33 @@ public abstract class AppDatabase extends RoomDatabase {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                                    AppDatabase.class, "app_database")
+                                    AppDatabase.class, "app_database").addCallback(sRoomDatabaseCallback)
                                 .build();
                     }
             }
         }
         return INSTANCE;
     }
+
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            // If you want to keep data through app restarts,
+            // comment out the following block
+            databaseWriteExecutor.execute(() -> {
+                // Populate the database in the background.
+                // If you want to start with more words, just add them.
+                PictureDetailDAO dao = INSTANCE.pictureDetailDAO();
+
+                PictureDetail pictureDetail = new PictureDetail("sfafasfsafs");
+                dao.insert(pictureDetail);
+                PictureDetail pictureDetail2 = new PictureDetail("Here is the code for creating the callback within the WordRoomDatabase class. Because you cannot do Room database operations on the UI thread, onCreate() uses the previously defined databaseWriteExecutor to execute a lambda on a background thread. The lambda deletes the contents of the database, then populates it with the two words \"Hello\" and \"World\". Feel free to add more words!");
+                dao.insert(pictureDetail2);
+            });
+        }
+    };
 
 }
--- app/src/main/java/com/example/sybrintextocr/ui/display/DisplayFragment.java
+++ app/src/main/java/com/example/sybrintextocr/ui/display/DisplayFragment.java
@@ -7,13 +7,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sybrintextocr.R;
+import com.example.sybrintextocr.database.PictureDetail;
 import com.example.sybrintextocr.databinding.FragmentDisplayBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -31,14 +37,31 @@ public class DisplayFragment extends Fragment {
         binding = FragmentDisplayBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Create the observer which updates the UI.
+        final Observer<List<PictureDetail>> observer = new Observer<List<PictureDetail>>() {
+            @Override
+            public void onChanged(@Nullable final List<PictureDetail> pictureDetail) {
+                // Update the UI, in this case, a TextView.
+                recyclerViewAdapter.notifyDataSetChanged();
+            }
+        };
+
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        displayViewModel.getPhotoDetail().observe( this.getViewLifecycleOwner(),observer);
 
 
+
+         List<PictureDetail> pics = new ArrayList<>();
+         pics.add(new PictureDetail("hjgjjhjh"));
+        pics.add(new PictureDetail("l,gfjydtydtu"));
 
         //Set up recyclerview
         RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
-        recyclerViewAdapter = new RecyclerViewAdapter(displayViewModel.getPhotoDetail().getValue());
+        recyclerViewAdapter = new RecyclerViewAdapter(pics);
         recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
         recyclerView.setAdapter(recyclerViewAdapter);
+
+
 
 
         return root;
